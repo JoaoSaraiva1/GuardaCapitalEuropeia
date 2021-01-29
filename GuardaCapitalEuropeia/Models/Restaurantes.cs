ff0cd148f9ef260a50e957a8855033920a6dee46
Vitor Pereira <vitor123@live.com.pt>
2021-01-29 12:30:27 +0000

    Validaçoes nos campos

--- GuardaCapitalEuropeia/Models/Restaurantes.cs
+++ GuardaCapitalEuropeia/Models/Restaurantes.cs
@@ -14,16 +14,23 @@ namespace GuardaCapitalEuropeia.Models
         [StringLength(100)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Por favor introduza a sua descrição")]
         public string Description { get; set; }
 
         public int Contacto { get; set; }
+        
+        [Required(ErrorMessage = "Por favor introduza o seu numero de telemóvel")]
+        [RegularExpression(("^[0-9]{9,9}$") , ErrorMessage = "Introduza o seu numero correto (9 Digitos)")]
+         public string Contact { get; set; }
 
-        public string Contact { get; set; }
-
+        [Required(ErrorMessage = "Por favor introduza o seu email")]
+        [RegularExpression(@"(\w+(\.\w+)*@[a-zA-Z_]+?\.[a-zA-Z]{2,6})", ErrorMessage = "Por favor introduza o email correto")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Por favor introduza a sua localização")]
         public string Location { get; set; }
 
+        [Required(ErrorMessage = "Por favor introduza a sua Lotação")]
         public int LimitMax { get; set; }
 
     }
