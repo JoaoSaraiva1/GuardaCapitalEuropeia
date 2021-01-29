5df720432469d99a5c732bc281b12d14d8271640
miguelpereira25 <mig_santop@hotmail.com>
2021-01-29 15:35:08 +0000

    Created the models for the registration of the tourists

---
+++ GuardaCapitalEuropeia/Models/EditLoggedInTouristViweModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitalEuropeia.Models
+{
+    public class EditLoggedInTouristViweModel
+    {
+        [Required]
+        [StringLength(128)]
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
