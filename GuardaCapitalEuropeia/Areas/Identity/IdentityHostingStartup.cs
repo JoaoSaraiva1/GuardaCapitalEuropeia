fb68d00692bbb87d457ec075614ce66eacbd2bb1
miguelpereira25 <mig_santop@hotmail.com>
2021-02-03 16:54:11 +0000

    Sca

---
+++ GuardaCapitalEuropeia/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GuardaCapitalEuropeia.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuardaCapitalEuropeia.Areas.Identity.IdentityHostingStartup))]
+namespace GuardaCapitalEuropeia.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
